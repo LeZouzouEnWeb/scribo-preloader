d9c10f052ca34f4f7addb691702ca4d8a293d565
Eric Corbisier <93837151+LeZouzouEnWeb@users.noreply.github.com>
2023-05-02 02:07:09 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+    $(window).on("load", function () {
+        // document.getElementById("chgt").style.display = "none";
+        $("#chgt").delay(500).fadeOut(1000);
+        $("#chgt-wrapper").delay(100).fadeOut(1400);
+        window.setTimeout(
+            // $("#chgt").html(""),
+            10000
+        );
+
+    });
+	});
